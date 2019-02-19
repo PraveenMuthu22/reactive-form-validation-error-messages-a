684b21012c1ac0f690a3b203ac73af41241d6516
Praveen Muthukumarana <praveenmuth2@gmail.com>
2019-02-19 17:34:37 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form-validation-a';
+  userForm: FormGroup;
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.userForm = this.fb.group({
+      name: ['', {
+        validators: Validators.required,
+        updateOn: 'blur',
+      }],
+      email: ['', {
+        validators: [Validators.required, Validators.pattern(this.emailRegex)],
+        updateOn: 'blur',
+      }],
+      phone: ['', {
+        validators: [Validators.minLength(10)],
+        updateOn: 'blur',
+      }]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
+
+  test() {
+    console.log(this.userForm);
+  }
 }
